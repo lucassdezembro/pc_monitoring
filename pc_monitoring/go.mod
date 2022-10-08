4b32018d98125a26fa64cff0c24903b5a461a190
Lucas Dezembro <87278285+lucassdezembro@users.noreply.github.com>
2022-10-08 20:27:16 -0300

    Delete pc_monitoring directory

--- pc_monitoring/go.mod
+++
@@ -1,5 +0,0 @@
-module personal/pc_monitoring
-
-go 1.19
-
-require github.com/mackerelio/go-osstat v0.2.3
--- pc_monitoring/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/lucassdezembro/pc_monitoring/pc_monitoring"
-
-func main() {
-	pc_monitoring.NewCpuMonitor()
-}
